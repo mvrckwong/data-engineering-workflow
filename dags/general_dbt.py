1f7c9bb5e8dfde1e1b77a028a690302f8a310639
Maverick Wong <mvrck.wong@gmail.com>
2025-03-08 20:00:26 +0800

    Update the pipeline

--- dags/general_dbt.py
+++ dags/general_dbt.py
@@ -1,11 +1,43 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
+from airflow.providers.slack.notifications.slack_webhook import \
+    send_slack_webhook_notification
+
+with DAG(
+    dag_id='dbt_test_all',
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,
+    on_success_callback=[on_success_callback],
+    tags=['dbt', 'tool-based']
+) as dag:
+
+	dbt_deps = BashOperator(
+		task_id='dbt_deps',
+		bash_command='cd /opt/airflow/dbt && dbt test'
+	)
+ 
+
+with DAG(
+    dag_id='dbt_run_all',
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,
+    on_success_callback=[on_success_callback],
+    tags=['dbt', 'tool-based']
+) as dag:
+
+	dbt_deps = BashOperator(
+		task_id='dbt_deps',
+		bash_command='cd /opt/airflow/dbt && dbt run'
+	)
+
 
 with DAG(
     dag_id='dbt_overall',
     start_date=datetime(2024, 1, 1),
-    schedule_interval=None
+    schedule_interval=None,
+    on_success_callback=[on_success_callback],
+    tags=['dbt', 'tool-based']
 ) as dag:
 
 	dbt_deps = BashOperator(
@@ -24,3 +56,44 @@ with DAG(
 	)
 
 	dbt_deps >> dbt_run >> dbt_test
+ 
+ 
+
+with DAG(
+    dag_id='dbt_layer_bronze',
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,
+    on_success_callback=[on_success_callback],
+    tags=['dbt', 'tool-based']
+) as dag:
+
+	dbt_deps = BashOperator(
+		task_id='dbt_deps',
+		bash_command='cd /opt/airflow/dbt && dbt run --select tag:bronze'
+	)
+ 
+with DAG(
+    dag_id='dbt_layer_silver',
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,
+    on_success_callback=[on_success_callback],
+    tags=['dbt', 'tool-based']
+) as dag:
+
+	dbt_deps = BashOperator(
+		task_id='dbt_deps',
+		bash_command='cd /opt/airflow/dbt && dbt run --select tag:silver'
+	)
+ 
+with DAG(
+    dag_id='dbt_layer_gold',
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,
+    on_success_callback=[on_success_callback],
+    tags=['dbt', 'tool-based']
+) as dag:
+
+	dbt_deps = BashOperator(
+		task_id='dbt_deps',
+		bash_command='cd /opt/airflow/dbt && dbt run --select tag:gold'
+	)
