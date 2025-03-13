5e49d4bf34f87eab0d33dffda92d4a4c4cf13be0
Maverick Wong <mvrck.wong@gmail.com>
2025-03-14 03:13:57 +0800

    Final gold layer tables and views

--- dbt/models/gold/dim_customers.sql
+++ dbt/models/gold/dim_customers.sql
@@ -2,27 +2,27 @@
     config(
         materialized='incremental',
         incremental_strategy='merge',
-        unique_key='store_key',
+        unique_key='customer_key',
         partition_by={
             "field": "valid_from",
             "data_type": "timestamp",
             "granularity": "day"
         },
-        cluster_by=['store_id', 'is_current'],
+        cluster_by=['customer_id', 'is_current'],
         dataset='gold'
     )
 }}
 
 select
-    {{ dbt_utils.generate_surrogate_key(['store_id', 'dbt_valid_from']) }} as store_key,
+    {{ dbt_utils.generate_surrogate_key(['customer_id', 'dbt_valid_from']) }} as customer_key,
     {{ dbt_utils.star(
-        from=ref('stg_adworks_stores_snapshot'),
+        from=ref('stg_adworks_customers_snapshot'),
         except=['dbt_valid_from', 'dbt_valid_to']
     ) }},
     dbt_valid_from as valid_from,
     dbt_valid_to as valid_to,
     (dbt_valid_to is null) as is_current
-from {{ ref('stg_adworks_stores_snapshot') }}
+from {{ ref('stg_adworks_customers_snapshot') }}
 
 {% if is_incremental() %}
     where dbt_valid_from > (select max(valid_from) from {{ this }})
