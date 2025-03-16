08a44ad9f36c2857dd351505a3d467ae05fed9fb
Maverick Wong <mvrck.wong@gmail.com>
2025-03-16 14:03:50 +0800

    Reformat gold models

--- dbt/models/gold/eg-adworks/dim_products_adworks.sql
+++ dbt/models/gold/eg-adworks/dim_products_adworks.sql
@@ -5,13 +5,13 @@
         unique_key='product_key',
         on_schema_change='sync_all_columns',
         partition_by={
-            "field": "_valid_from", 
-            "data_type": "timestamp", 
-            "granularity": "day"
+            "field": "_valid_from"
+            , "data_type": "timestamp"
+            , "granularity": "day"
         },
         cluster_by=[
-            'product_key', 
-            '_is_current'
+            'product_key'
+            , '_is_current'
         ],
         tags=['eg']
     )
@@ -19,8 +19,8 @@
 
 WITH source AS (
     SELECT
-        {{ dbt_utils.star(from=ref('snap_products_adworks')) }}, 
-        (dbt_valid_to IS NULL) AS is_current
+        {{ dbt_utils.star(from=ref('snap_products_adworks')) }}
+        , (dbt_valid_to IS NULL) AS is_current
     FROM 
         {{ ref('snap_products_adworks') }}
     
@@ -34,16 +34,16 @@ WITH source AS (
 
 -- Final dimension table with enriched attributes
 SELECT
-    {{ dbt_utils.generate_surrogate_key(['s.product_id', 's.dbt_valid_from']) }} AS product_key,
-    {{ dbt_utils.star(
+    {{ dbt_utils.generate_surrogate_key(['s.product_id', 's.dbt_valid_from']) }} AS product_key
+    , {{ dbt_utils.star(
         from=ref('snap_products_adworks'), 
         relation_alias='s', 
         except=['dbt_valid_from', 'dbt_valid_to']
-    ) }},
+    ) }}
     
     -- SCD metadata
-    s.dbt_valid_from AS _valid_from,
-    s.dbt_valid_to AS _valid_to,
-    s.is_current AS _is_current
+    , s.dbt_valid_from AS _valid_from
+    , s.dbt_valid_to AS _valid_to
+    , s.is_current AS _is_current
 
 FROM source s
--- dbt/models/gold/eg-adworks/dim_stores_adworks.sql
+++ dbt/models/gold/eg-adworks/dim_stores_adworks.sql
@@ -5,13 +5,13 @@
         unique_key='store_key',
         on_schema_change='sync_all_columns',
         partition_by={
-            "field": "_valid_from", 
-            "data_type": "timestamp", 
-            "granularity": "day"
+            "field": "_valid_from"
+            , "data_type": "timestamp"
+            , "granularity": "day"
         },
         cluster_by=[
-            'store_key', 
-            '_is_current'
+            'store_key'
+            , '_is_current'
         ],
         tags=['eg']
     )
@@ -19,8 +19,8 @@
 
 WITH source AS (
     SELECT
-        {{ dbt_utils.star(from=ref('snap_stores_adworks')) }}, 
-        (dbt_valid_to IS NULL) AS is_current
+        {{ dbt_utils.star(from=ref('snap_stores_adworks')) }}
+        , (dbt_valid_to IS NULL) AS is_current
     FROM 
         {{ ref('snap_stores_adworks') }}
     
@@ -34,16 +34,16 @@ WITH source AS (
 
 -- Final dimension table with enriched attributes
 SELECT
-    {{ dbt_utils.generate_surrogate_key(['s.store_id', 's.dbt_valid_from']) }} AS store_key,
-    {{ dbt_utils.star(
+    {{ dbt_utils.generate_surrogate_key(['s.store_id', 's.dbt_valid_from']) }} AS store_key
+    , {{ dbt_utils.star(
         from=ref('snap_stores_adworks'), 
         relation_alias='s', 
         except=['dbt_valid_from', 'dbt_valid_to']
-    ) }},
+    ) }}
     
     -- SCD metadata
-    s.dbt_valid_from AS _valid_from,
-    s.dbt_valid_to AS _valid_to,
-    s.is_current AS _is_current
+    , s.dbt_valid_from AS _valid_from
+    , s.dbt_valid_to AS _valid_to
+    , s.is_current AS _is_current
 
 FROM source s
