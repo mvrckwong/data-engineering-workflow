dbc67d7cd665a07d49e15ca9b8d6ea134cdeee30
Maverick Wong <mvrck.wong@gmail.com>
2025-03-16 19:33:26 +0800

    Format the dimension tables

--- dbt/models/silver/eg-adworks/stg_regions_adworks.sql
+++ dbt/models/silver/eg-adworks/stg_regions_adworks.sql
@@ -12,37 +12,40 @@
     )
 }}
 
--- insert general transformations here,
--- including joining, cleaning, type conversion, renaming
--- including testing, validation processes
-
 WITH source AS (
     SELECT
         *
-        , CURRENT_TIMESTAMP() AS _extrated_date
     FROM 
         {{ ref('raw_regions_adworks') }}
+    
+    {% if is_incremental() %}
+    WHERE
+        -- This assumes your source data has an updated_at or similar field
+        -- Change the field name to match your actual data
+        -- updated_at > (
+        --     SELECT MAX(updated_at) 
+        --     FROM {{ this }}
+        -- )
+        
+        -- Alternatively, if you don't have an updated_at field:
+        region_id NOT IN (
+            SELECT region_id 
+            FROM {{ this }}
+        )
+    {% endif %}
+),
+
+transformed_source AS (
+    SELECT
+        CAST(region_id AS INT64) AS region_id
+        , sales_district
+        , sales_region
+        , CURRENT_TIMESTAMP() AS _extrated_date
+    FROM 
+        source
 )
 
 SELECT 
     * 
 FROM 
-    source
-
-{% if is_incremental() %}
-
-WHERE
-    -- This assumes your source data has an updated_at or similar field
-    -- Change the field name to match your actual data
-    -- updated_at > (
-    --     SELECT MAX(updated_at) 
-    --     FROM {{ this }}
-    -- )
-    
-    -- Alternatively, if you don't have an updated_at field:
-    region_id NOT IN (
-        SELECT region_id 
-        FROM {{ this }}
-    )
-
-{% endif %}
+    transformed_source
--- dbt/models/silver/eg-adworks/stg_stores_adworks.sql
+++ dbt/models/silver/eg-adworks/stg_stores_adworks.sql
@@ -12,39 +12,49 @@
         tags=['eg']
     )
 }}
-
--- insert general transformations here,
--- including joining, cleaning, type conversion, renaming
--- including testing, validation processes
 -- workflow: raw -> silver -> snapshot -> gold
-
 WITH source AS (
     SELECT
         *
-        , CURRENT_TIMESTAMP() AS _extrated_date
     FROM 
         {{ ref('raw_stores_adworks') }}
+    
+    {% if is_incremental() %}
+    WHERE
+        -- This assumes your source data has an updated_at or similar field
+        -- Change the field name to match your actual data
+        -- updated_at > (
+        --     SELECT MAX(updated_at) 
+        --     FROM {{ this }}
+        -- )
+        
+        -- Alternatively, if you don't have an updated_at field:
+        store_id NOT IN (
+            SELECT store_id 
+            FROM {{ this }}
+        )
+    {% endif %}
+),
+transformed_source AS (
+    SELECT
+        CAST(store_id AS INT64) AS store_id
+        , CAST(region_id AS INT64) AS region_id
+        , store_type
+        , store_name
+        , store_street_address
+        , store_city
+        , store_state
+        , store_country
+        , store_phone
+        , PARSE_DATE('%m/%d/%Y', first_opened_date) AS first_opened_date
+        , PARSE_DATE('%m/%d/%Y', last_remodel_date) AS last_remodel_date
+        , CAST(total_sqft AS INT64) AS total_sqft
+        , CAST(grocery_sqft AS INT64) AS grocery_sqft
+        , CURRENT_TIMESTAMP() AS _extrated_date
+    FROM 
+        source
 )
-
 SELECT 
     * 
 FROM 
-    source
-
-{% if is_incremental() %}
-
-WHERE
-    -- This assumes your source data has an updated_at or similar field
-    -- Change the field name to match your actual data
-    -- updated_at > (
-    --     SELECT MAX(updated_at) 
-    --     FROM {{ this }}
-    -- )
-    
-    -- Alternatively, if you don't have an updated_at field:
-    store_id NOT IN (
-        SELECT store_id 
-        FROM {{ this }}
-    )
-
-{% endif %}
+    transformed_source
