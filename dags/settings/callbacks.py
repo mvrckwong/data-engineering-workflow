b64e1f29e1575ef873dd95a5c5342cf10e1c83c9
Maverick Wong <mvrck.wong@gmail.com>
2025-05-11 18:00:13 +0800

    Update for notification

--- dags/settings/callbacks.py
+++ dags/settings/callbacks.py
@@ -2,34 +2,42 @@ import requests
 from airflow.models import Variable
 
 
-class AirflowChatNotifier:
+class GoogleChatNotifier:
 	def __init__(self):
 		"""Initialize the AirflowChatNotifier class."""
 		self.webhook_url = Variable.get("WEBHOOK_GCHAT_URL")
 		if not self.webhook_url:
 			raise ValueError("Webhook URL is not set in Airflow Variables.")
 		
-	# TODO: Add more methods to handle different types of notifications.
-	# Data pipeline or task has failed.
-	# Data pipeline or task has succeeded.
-	# Data pipeline or task on retry.
+	"""
+	TODO: Add more methods to handle different types of notifications.
+	- on_failure: Send a notification to Google Chat on task failure.
+	- on_success: Send a notification to Google Chat on task success.
+	- on_retry: Send a notification to Google Chat on task retry.
+	"""
+
+	def on_success(self, context):
+		"""Send a notification to Google Chat on task failure."""
+		payload = {
+			"text": "\n".join([
+				"Data pipeline or task has succeeded.",
+				"No context available."
+			])
+		}
+
+		return requests.post(self.webhook_url, json=payload)
 
 	def on_failure(self, context):
 		"""Send a notification to Google Chat on task failure."""
 		payload = {
 			"text": "\n".join([
-				"Data pipeline or task has failed."
+				"Data pipeline or task has failed.",
+				"No context available."
 			])
 		}
+
+		return requests.post(self.webhook_url, json=payload)
 	
-		try:
-			response = requests.post(
-				self.webhook_url,
-				json=payload
-			)
-			print(f"Status code: {response.status_code}")
-			print(f"Response text: {response.text}")
-			return response.status_code
-		except Exception as e:
-			print(f"Exception occurred: {str(e)}")
-			return None
+
+if __name__ == "__main__":
+	None
